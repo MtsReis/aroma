6c64f99dc87398b4c81cf52e9382ace81e834f94
Matheus R <notcolondee@gmail.com>
2023-08-04 00:41:08 -0300

    Avoid unnecessary imports into _ENV._G

--- conf.lua
+++ conf.lua
@@ -1,12 +1,14 @@
--- Include some necessary libs
-require 'lib/pl'
-require("lib/stateManager")
-require("lib/lovelyMoon")
-lip = require 'lib/LIP'
-log = require("lib/log")
-pd = pretty.dump
-pw = pretty.write
-stringx.import()
+-- Core libs
+pl = require'lib.pl.import_into'() -- On-demand lib loading into pl
+lip = require 'lib.LIP'
+log = require 'lib.log'
+require 'lib.stateManager'
+require 'lib.lovelyMoon'
+
+-- Direct access to some convenient functions
+pd = require 'lib.pl.pretty'.dump
+pw = require 'lib.pl.pretty'.write
+pl.stringx.import() -- Bring the stringx methods into the standard string table
 
 -- Init specific config
 log.level = "warn"
