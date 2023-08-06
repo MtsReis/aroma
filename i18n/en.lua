abf57433678e6cdcb13489e46b2bbe6649b1e0bc
Matheus R <notcolondee@gmail.com>
2023-08-06 00:34:45 -0300

    Add initial translation files

---
+++ i18n/en.lua
@@ -0,0 +1,15 @@
+return {
+  en = {
+    UNUSED_BLANK_MSG_EXAMPLE_btn = " ",
+
+    FILE = "File",
+    VIEW = "View",
+    OPTIONS = "Options",
+    QUIT = "Quit",
+    FULLSCREEN = "Fullscreen",
+
+    -- Temporary or unimportant. No need to translate these.
+    FLASHBANG = "Flashbang!",
+    TEST_WINDOW = "Test window"
+  }
+}
---
+++ i18n/pt-BR.lua
@@ -0,0 +1,14 @@
+return {
+  pt = {
+    UNUSED_BLANK_MSG_EXAMPLE_btn = " ",
+
+    FILE = "Arquivo",
+    OPTIONS = "Opções",
+    QUIT = "Sair",
+    FULLSCREEN = "Tela cheia",
+
+    -- Temporary or unimportant. No need to translate these.
+    FLASHBANG = "Clarão!",
+    TEST_WINDOW = "Janela de teste"
+  }
+}
