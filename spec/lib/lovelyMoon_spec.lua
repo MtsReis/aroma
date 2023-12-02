d9b21dcda7394577996552095f6cfe26714d2ee7
Mts <notcolondee@gmail.com>
2023-12-01 23:13:40 -0300

    Remove failing test

--- spec/lib/lovelyMoon_spec.lua
+++ spec/lib/lovelyMoon_spec.lua
@@ -2,8 +2,4 @@ describe("[#lib] lovelyMoon", function()
   describe("by updating a state", function()
     pending("should call the methods in order")
   end)
-
-  it("should fail this test", function()
-    assert.is_true(false)
-  end)
 end)
