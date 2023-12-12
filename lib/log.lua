dbcbcb85fe13ae4d7b1c1ec1942644984b5423a6
Mts <notcolondee@gmail.com>
2023-12-11 22:44:36 -0300

    Add support to include traceback to logs

--- lib/log.lua
+++ lib/log.lua
@@ -12,57 +12,63 @@ local log = { _version = "0.1.0" }
 log.usecolor = true
 log.outfile = "log"
 log.level = "trace"
+log.tbLevel = false
 
 
 local modes = {
-	{ name = "trace", termColor = "\27[34m", rgba = {52/255, 101/255, 164/255, 1}},
-	{ name = "debug", termColor = "\27[36m", rgba = {6/255, 152/255, 154/255, 1}},
-	{ name = "info",  termColor = "\27[32m", rgba = {78/255, 154/255, 6/255, 1}},
-	{ name = "warn",  termColor = "\27[33m", rgba = {196/255, 160/255, 0, 1}},
-	{ name = "error", termColor = "\27[31m", rgba = {204/255, 0, 0, 1}},
-	{ name = "fatal", termColor = "\27[35m", rgba = {117/255, 80/255, 123/255, 1}},
+  { name = "trace", termColor = "\27[34m", rgba = {52/255, 101/255, 164/255, 1}},
+  { name = "debug", termColor = "\27[36m", rgba = {6/255, 152/255, 154/255, 1}},
+  { name = "info",  termColor = "\27[32m", rgba = {78/255, 154/255, 6/255, 1}},
+  { name = "warn",  termColor = "\27[33m", rgba = {196/255, 160/255, 0, 1}},
+  { name = "error", termColor = "\27[31m", rgba = {204/255, 0, 0, 1}},
+  { name = "fatal", termColor = "\27[35m", rgba = {117/255, 80/255, 123/255, 1}},
 }
 
 
 local levels = {}
 for i, v in ipairs(modes) do
-	levels[v.name] = i
+  levels[v.name] = i
 end
 
 
 local round = function(x, increment)
-	increment = increment or 1
-	x = x / increment
-	return (x > 0 and math.floor(x + .5) or math.ceil(x - .5)) * increment
+  increment = increment or 1
+  x = x / increment
+  return (x > 0 and math.floor(x + .5) or math.ceil(x - .5)) * increment
 end
 
 
 local _tostring = tostring
 
 local tostring = function(...)
-	local t = {}
-	for i = 1, select('#', ...) do
-		local x = select(i, ...)
-		if type(x) == "number" then
-			x = round(x, .01)
-		end
-		t[#t + 1] = _tostring(x)
-	end
-	return table.concat(t, " ")
+  local t = {}
+  for i = 1, select('#', ...) do
+    local x = select(i, ...)
+    if type(x) == "number" then
+      x = round(x, .01)
+    end
+    t[#t + 1] = _tostring(x)
+  end
+  return table.concat(t, " ")
 end
 
 for i, x in ipairs(modes) do
-	local nameupper = x.name:upper()
-	log[x.name] = function(...)
+  local nameupper = x.name:upper()
+  log[x.name] = function(...)
 
-		-- Return early if we're below the log level
-		if i < levels[log.level] then
-			return
-		end
+    -- Return early if we're below the log level
+    if i < levels[log.level] then
+      return
+    end
 
-		local msg = tostring(...)
-		local info = debug.getinfo(2, "Sl")
-		local lineinfo = info.short_src .. ":" .. info.currentline
+    local msg = tostring(...)
+    local info = debug.getinfo(2, "Sl")
+    local lineinfo = info.short_src .. ":" .. info.currentline
+
+    -- Append traceback if tbLevel is defined and level is above it
+    if (log.tbLevel and type(levels[log.tbLevel]) == "number" and i >= levels[log.tbLevel]) then
+      msg = msg .. "\n" .. debug.traceback()
+    end
 
     -- Output to console
     print(string.format("%s[%-6s%s]%s %s: %s",
@@ -73,13 +79,12 @@ for i, x in ipairs(modes) do
                         lineinfo,
                         msg))
 
-		-- Output to log file
-		if log.outfile then
-				local str = string.format("[%-6s%s] %s: %s\n", nameupper, os.date(), lineinfo, msg)
-				love.filesystem.append(log.outfile, str)
-		end
-	end
+    -- Output to log file
+    if log.outfile then
+        local str = string.format("[%-6s%s] %s: %s\n", nameupper, os.date(), lineinfo, msg)
+        love.filesystem.append(log.outfile, str)
+    end
+  end
 end
-
 
 return log
---
+++ spec/lib/log_spec.lua
@@ -0,0 +1,90 @@
+local log = require "lib.log"
+local match = require("luassert.match")
+
+local levels = {"trace", "debug", "info", "warn", "error", "fatal"}
+local tbLevel = 4 -- warn
+
+local function in_text(state, arguments)
+  local expected = arguments[1]
+
+  return function(value)
+    return value:find(expected) ~= nil
+  end
+end
+
+assert:register("matcher", "in_text", in_text)
+
+describe("[#lib] log.lua", function()
+  setup(function()
+    -- Dummies for dependencies
+    _G.love = {
+      filesystem = {
+        append = function() end
+      }
+    }
+
+    stub(_G, "print")
+    stub(love.filesystem, "append")
+
+    log.outfile = "fileName"
+  end)
+
+  describe("by calling a log level", function()
+    before_each(function()
+      love.filesystem.append:clear()
+      print:clear()
+    end)
+
+    it("should print the message to the standard output", function()
+      log[levels[1]]("Message")
+      assert.stub(print).was.called_with(match.is.in_text("Message"))
+    end)
+
+    it("should append the message to the log file through love.filesystem", function()
+      log[levels[1]]("Another message")
+      assert.stub(love.filesystem.append).was.called_with(match.is_equal("fileName"), match.is.in_text("Another message"))
+    end)
+
+    it("should NOT log lower levels", function()
+      log.level = levels[3]
+      log[levels[2]]("New message")
+
+      assert.stub(print).was.not_called()
+    end)
+
+    it("should include a traceback ONLY for levels equal or higher than the one defined in log.tbLevel", function()
+      local s = spy.on(debug, "traceback")
+
+      log.tbLevel = levels[tbLevel]
+      log.level = levels[tbLevel]
+
+      log[levels[tbLevel]]("Detailed message")
+      log[levels[tbLevel + 1]]("Detailed message with higher level")
+
+      assert.spy(s).was.called(2)
+      debug.traceback:clear()
+
+      log[levels[tbLevel - 1]]("Simple message")
+      assert.spy(s).was.not_called()
+
+      debug.traceback:revert()
+    end)
+
+    it("should NEVER include a traceback if log.tbLevel is false", function()
+      local s = spy.on(debug, "traceback")
+
+      log.tbLevel = false
+      log.level = levels[#levels]
+
+      log[levels[#levels]]("Highest level message")
+      assert.spy(s).was.not_called()
+
+      debug.traceback:revert()
+    end)
+  end)
+
+  teardown(function()
+    print:revert()
+    love.filesystem.append:revert()
+  end)
+end)
